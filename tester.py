3e329717833915189ea9e97bdd8ff27aee3d245a
zhangchi_office <zhangchi0812@163.com>
2018-03-12 14:57:05 +0800

    3-12pm
    Signed-off-by: zhangchi_office <zhangchi0812@163.com>

--- tester.py
+++ tester.py
@@ -71,4 +71,49 @@
 # fnord *= 2
 # print(fnord)
 
-#
+#######################下面插入本地电脑中的内容
+
+#布尔运算符
+# number = int(input("Enter a number between 1 and 10"))
+# if number <= 10:
+#     if number >=1:
+#         print("Great!")
+#     else:
+#         print("Wrong!")
+# else:
+#     print("Wrong!")
+# #方法二
+# if 1 <= number <= 10:
+#     print("0-1")
+# else:
+#     print("error")
+
+# #简单if语句书写方式
+# b = int(input("输入数字"))
+# a if b < 10 else c#如果b<10则返回a，否则返回c
+
+# #断言 在调试过程中使用较多 可以用于判断条件是否为真
+# age = 10
+# assert 0 < age < 100
+# age = 101
+# assert 0 < age < 100, 'The age must be realistic'#条件后可以增加字符串，用于解释断言
+
+# #while循环 当条件为真的情况时重复执行一段代码 可以使用
+# #打印1-100的数字
+# x = 1
+# while x <= 100:
+#     print(x)
+#     x += 1
+
+# #判断name中填写数据，否则返回重新填写
+# name = ""
+# # while not name or name.isspace():#isspace() 方法检测字符串是否只由空白字符组成。
+# while not name.strip():#strip函数用于移除字符串首尾指定字符，默认为空格
+#     name = str(input("Please enter your name"))
+# print("Hello, %s!" % name)
+
+# #for循环 要为一个集合的每个元素都执行相同代码时使用
+# numbers = range(10)
+# for number in numbers:
+#     print(number)
+
